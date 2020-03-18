10f74f5dedd9c2b725b6a4fe2cdccacd0d804ef4
Feodor Fitsner <feodor@appveyor.com>
2020-03-18 12:26:27 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocaleNetFxTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Dictionary<string, string> localeAndExpectedFormattedNumbers = new Dictionary<string, string>()
+		{
+			{ "bg-BG", "12,35" },
+			{ "de-CH", "12.35" },
+			{ "fr-CH", "12,35" }
+
+		};
+
+			foreach (string locale in localeAndExpectedFormattedNumbers.Keys)
+			{
+				string expectedFormat = localeAndExpectedFormattedNumbers[locale];
+				string formattedNumber = string.Format(new CultureInfo(locale), "{0}", 12.35);
+
+				string sameValue = expectedFormat == formattedNumber ? "OK" : "NOT";
+
+				Console.WriteLine("Locale '{0}', expected: {1}, formatted: {2} ({3})", locale, expectedFormat, formattedNumber, sameValue);
+			}
+		}
+    }
+}
